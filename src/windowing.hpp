e387cfc471f2f5534a99fca10ae87cd98aebc3b9
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-13 15:22:51 -0400

    Fix(windowing): Removed windowing.cpp due to linking issues

--- src/windowing.hpp
+++ src/windowing.hpp
@@ -10,9 +10,9 @@ extern "C" {
 
 extern HWND main_window_handle;
 
-LRESULT Wndproc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void open_main_window(LPCTSTR window_title, HICON window_icon, HICON small_window_icon, COLORREF background_color, WNDPROC window_process);
 
+LRESULT WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 #ifdef __cplusplus
 }
 #endif

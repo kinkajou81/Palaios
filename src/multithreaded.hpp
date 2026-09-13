ec400de7baea45f2b7795dbc22ccd4babea23538
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-13 15:55:04 -0400

    Feat(multithreaded): Added function main_window

--- src/multithreaded.hpp
+++ src/multithreaded.hpp
@@ -12,4 +12,17 @@ namespace multithreaded {
         COLORREF background_color;
         WNDPROC window_process;
     } main_window_args;
+
+    DWORD __stdcall main_window(LPVOID lpThreadParameter) {
+        main_window_args* args = (main_window_args*)lpThreadParameter;
+        open_main_window(args->window_title, args->window_icon, args->small_window_icon, args->background_color, args->window_process);
+        register_raw_input_devices(main_window_handle);
+        
+        MSG msg = {};
+        while (GetMessage(&msg, nullptr, 0, 0)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        return 0;
+    }
 }

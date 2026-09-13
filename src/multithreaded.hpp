c193537357b4071817570d07dbe97056f833d24b
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-13 15:54:38 -0400

    Feat(multithreaded): Added struct main_window_args

--- src/multithreaded.hpp
+++ src/multithreaded.hpp
@@ -5,4 +5,11 @@
 #include <windows.h>
 
 namespace multithreaded {
+    typedef struct {
+        LPCTSTR window_title;
+        HICON window_icon;
+        HICON small_window_icon;
+        COLORREF background_color;
+        WNDPROC window_process;
+    } main_window_args;
 }

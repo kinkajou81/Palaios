71a2b48e0383af0ae4b1b9faa246f11b2299df2d
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-13 15:28:46 -0400

    Refactor(windowing): Renamed windowing.hpp to windowing.h
    Reason is to be consistent with the source being a .c file

--- src/windowing.h
+++ src/windowing.h
@@ -13,6 +13,9 @@ extern HWND main_window_handle;
 void open_main_window(LPCTSTR window_title, HICON window_icon, HICON small_window_icon, COLORREF background_color, WNDPROC window_process);
 
 LRESULT WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+bool launched_from_console();
+void hide_terminal();
+
 #ifdef __cplusplus
 }
 #endif

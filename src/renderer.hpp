cf9469752ce645031f738fed2b2945f6cdcf509f
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-13 20:22:45 -0400

    Feat(renderer): Added struct background_control

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -18,3 +18,12 @@ typedef struct {
     bool display_object_windows : 1;
 } display_control;
 
+typedef struct {
+    uintptr_t tile_data_base;
+    uintptr_t map_data_base;
+    uint16_t x_offset;
+    uint16_t y_offset;
+    uint8_t priority : 5;
+    bool mosaic : 1;
+} background_control;
+

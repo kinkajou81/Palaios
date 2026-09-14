6fc82f5f0100e8feb5a2527bb78d520da5827846
kinkajou81 <132849313+kinkajou81@users.noreply.github.com>
2026-09-13 20:22:31 -0400

    Feat(renderer): Added struct display_control

--- src/renderer.hpp
+++ src/renderer.hpp
@@ -4,3 +4,17 @@
 
 extern uint16_t verical_counter;
 
+typedef struct {
+    uint8_t brightness;
+    bool display_background_0 : 1;
+    bool display_background_1 : 1;
+    bool display_background_2 : 1;
+    bool display_background_3 : 1;
+    bool display_background_4 : 1;
+    bool display_background_5 : 1;
+    bool display_objects : 1;
+    bool display_window_0 : 1;
+    bool display_window_1 : 1;
+    bool display_object_windows : 1;
+} display_control;
+
